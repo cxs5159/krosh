0e043642fb3a2f36559d119df9ae4c0c71a1aa2f
Rahul Johri <rahul.johri@finra.org>
2017-09-14 11:03:36 -0400

    Pushing the new project

---
+++ KeywestKrosh/src/main/java/org/createathon/krosh/IRAD.java
@@ -0,0 +1,13 @@
+package org.createathon.krosh;
+
+public class IRAD {
+    public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ KeywestKrosh/src/main/java/org/createathon/krosh/KeywestKrosh.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.createathon.krosh;
+
+/**
+ * @author K25176
+ *
+ */
+public class KeywestKrosh {
+
+	public static void main(String args[]){
+		
+		System.out.println("Hello Guys");
+	}
+	
+}
